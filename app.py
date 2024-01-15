cc3d0b376380964963b027db86c7e3427d5c7e39
isaacwach <isaacwachira57@gmail.com>
2024-01-15 09:17:45 +0300

    created a condition to start the car or tell the user the car is already started if already started

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ command = ""
 car_started = False
 while True:
     command = input("> ").lower()
+    if command == "start":
+        if car_started:
+            print("The car is already started")
+        else:
+            print("Car started...")
+            car_started = True
